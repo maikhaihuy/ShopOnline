4f4c9d499303059a98aa40e2523fda1bd9579ddb
anhhong1803 <ttahong1803@gmail.com>
2015-12-24 01:41:22 +0700

    Add functions to Product dao

--- src/main/java/com/h2/model/dao/implementation/ProductDaoImp.java
+++ src/main/java/com/h2/model/dao/implementation/ProductDaoImp.java
@@ -1,41 +1,87 @@
 package com.h2.model.dao.implementation;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.h2.model.dao.interfaces.AbstractHbnDao;
+import com.h2.model.dao.interfaces.CategoryDao;
 import com.h2.model.dao.interfaces.ProductDao;
+import com.h2.model.pojo.Category;
 import com.h2.model.pojo.Product;
 
 @Repository ("productDao")
 @Transactional
 public class ProductDaoImp extends AbstractHbnDao<Product> implements ProductDao {
-
+	@Autowired
+	private CategoryDao categoryDao;
+	
 	public List<Product> getListProduct() {
 		return getAll(Product.class.getName());
 	}
 
 	public Product getProductById(int productId) {
 		// TODO Auto-generated method stub
-		return null;
+		return get(productId, Product.class);
 	}
 
 	public List<Product> getListNewProduct() {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = null;
+        List<Product> listProduct = new ArrayList<Product>();
+        String hql = "";      
+        
+        try{                	
+            hql = "FROM Product p WHERE p.productNew = :productNew ORDER BY p.productName";
+            query = getSession().createQuery(hql);
+            query.setParameter("productNew", 1);
+            listProduct =  query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            //log.error(e);          
+        } 
+        return listProduct;
 	}
 
 	public List<Product> getListDiscountProduct() {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = null;
+        List<Product> listProduct = new ArrayList<Product>();
+        String hql = "";      
+        
+        try{                	
+            hql = "FROM Product p WHERE p.productNew = :productNew ORDER BY p.productName";
+            query = getSession().createQuery(hql);
+            query.setParameter("productNew", 1);
+            listProduct =  query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            //log.error(e);          
+        } 
+        return listProduct;
 	}
 
-	public List<Product> getListProductByIdCategoryProductName(int categoryId,
-			String productName) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Product> getListProductByIdCategoryProductName(int categoryId, String productName) {
+		Query query = null;
+        List<Product> listProduct = new ArrayList<Product>();
+        String hql = "";    
+        
+        // Get category by categoryId
+        Category category = categoryDao.get(categoryId, Category.class);
+        
+        try{                	
+            hql = "FROM Product p WHERE p.productName LIKE :productName AND p.category = :category ORDER BY p.productName";
+            query = getSession().createQuery(hql);   
+            query.setString("productName", "%"+productName+"%");
+            query.setParameter("category", category);
+            listProduct =  query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            //log.error(e);          
+        } 
+        return listProduct;
 	}
 	
 }
---
+++ src/test/java/com/h2/model/dao/implementation/ProductDaoImpTest.java
@@ -0,0 +1,71 @@
+package com.h2.model.dao.implementation;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.h2.model.dao.interfaces.ProductDao;
+import com.h2.model.pojo.Product;
+
+public class ProductDaoImpTest extends TestCase{
+	
+	@Test
+    public void testGetProductById() {
+        System.out.println("getProductById");
+        ApplicationContext appCtx = new ClassPathXmlApplicationContext("beans-service.xml");
+		ProductDao instance = (ProductDao)appCtx.getBean("productDao");
+		
+        Product expResult = null;      
+        Product result = instance.getProductById(1);
+        if (result == expResult){
+            fail("The test case is a prototype.");
+        }
+        System.out.println("productName: " + result.getProductName());
+    }
+	
+	@Test
+    public void testGetListProduct() {
+        System.out.println("getListProduct");
+        ApplicationContext appCtx = new ClassPathXmlApplicationContext("beans-service.xml");
+		ProductDao instance = (ProductDao)appCtx.getBean("productDao");
+		
+        List<Product> expResult = null;      
+        List<Product> result = instance.getListProduct();
+        if (result == expResult){
+            fail("The test case is a prototype.");
+        }
+        System.out.println("list size: " + result.size());
+    }
+	
+	@Test
+    public void testGetListNewProduct() {
+        System.out.println("getListNewProduct");
+        ApplicationContext appCtx = new ClassPathXmlApplicationContext("beans-service.xml");
+		ProductDao instance = (ProductDao)appCtx.getBean("productDao");
+		
+        List<Product> expResult = null;      
+        List<Product> result = instance.getListNewProduct();
+        if (result == expResult){
+            fail("The test case is a prototype.");
+        }
+        System.out.println("list size: " + result.size());
+    }
+	
+	@Test
+    public void testGetListProductByIdCategoryProductName() {
+        System.out.println("getListListProductByIdCategoryProductName");
+        ApplicationContext appCtx = new ClassPathXmlApplicationContext("beans-service.xml");
+		ProductDao instance = (ProductDao)appCtx.getBean("productDao");
+		
+        List<Product> expResult = null;      
+        List<Product> result = instance.getListProductByIdCategoryProductName(1, "pro");
+        if (result == expResult){
+            fail("The test case is a prototype.");
+        }
+        System.out.println("list size: " + result.size());
+    }
+}
